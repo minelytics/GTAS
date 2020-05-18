625424777dc7002fefbd68f12e4d0fc5b9ccfd3a
Maanas Royy <m4manas@gmail.com>
2020-05-18 14:45:20 +0800

    Added structure for the generators

---
+++ gtas/generator/management/commands/generate_api.py
@@ -0,0 +1,15 @@
+from django.core.management.base import BaseCommand, CommandError
+from gtas.generator.workflow.api.generate_api import GenerateApi
+from time import time
+
+class Command(BaseCommand):
+    help = "Generate the api files for GTAS"
+
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **options):
+        start_time = time()
+        task = GenerateApi()
+        task.run(task=None, transaction=None)
+        print("--- %s seconds ---" % (time() - start_time))

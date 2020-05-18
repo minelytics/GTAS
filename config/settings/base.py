017079562ebb8a9a8f33f18cb7e006f56fe24872
Maanas Royy <m4manas@gmail.com>
2020-05-18 14:50:53 +0800

    Added more structure for the application

--- config/settings/base.py
+++ config/settings/base.py
@@ -76,6 +76,8 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     "gtas.users.apps.UsersConfig",
     # Your stuff: custom apps go here
+    "gtas.generator.apps.GeneratorConfig",
+
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS

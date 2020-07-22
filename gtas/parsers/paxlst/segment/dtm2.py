b1c1af18ea78ccfb6d6ec0c2eef89df0904c6717
Arun Chahar <arunkchahar@minelytics.in>
2020-07-22 15:46:09 +0530

    REFACTOR(*): remove constructor (__init__()) from DTM

--- gtas/parsers/paxlst/segment/dtm2.py
+++ gtas/parsers/paxlst/segment/dtm2.py
@@ -2,26 +2,23 @@ from gtas.parsers.paxlst.element2 import Element
 
 
 class DTM:
-    def __init__(self, message, group=None):
-        self.elements = message.segments[0].elements
-        self.group = group
-
-    @property
-    def parse(self):
+    def parse(self, message, group=None):
+        elements = message.segments[0].elements
         e = Element()
+
         output = {
             "segment": "DTM",
             "segment_description": "Date/Time/Period",
-            "group": self.group,
+            "group": group,
         }
-        output.update(e.segment_group("DTM", self.group))
+        output.update(e.segment_group("DTM", group))
 
         output["elements"].append(
             e.definition(
                 data_element="C507",
                 component_element="2005M",
                 attributes="an..3",
-                data=self.elements[0][0],
+                data=elements[0][0],
             )
         )
         output["elements"].append(
@@ -29,17 +26,17 @@ class DTM:
                 data_element="C507",
                 component_element="2380M",
                 attributes="n..10",
-                data=self.elements[0][1],
+                data=elements[0][1],
             )
         )
 
-        if e.struct(self.elements) == "list(list(str,str,str))":
+        if e.struct(elements) == "list(list(str,str,str))":
             output["elements"].append(
                 e.definition(
                     data_element="C507",
                     component_element="2379C",
                     attributes="an..3",
-                    data=self.elements[0][2],
+                    data=elements[0][2],
                 )
             )
 
